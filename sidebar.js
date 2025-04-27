cc1cdfcbe388eb398b99d6d38217124a5db26aa6
madhavimistry <madhavimistry237@gmail.com>
2025-04-27 21:04:32 +0530

    make component changes

---
+++ sidebar.js
@@ -0,0 +1,59 @@
+class SideBar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+
+        <div class="sidebar-panel">
+            <div class="m-item">
+                <a href="index" class="nav-item">
+                    <img src="./images/home.svg" class="v_panel" alt="home">
+                    <h5 class="mb-0">Home</h5>
+                </a>
+            </div>
+            <div class="m-item">
+                <a href="#" class="nav-item">
+                    <img src="./images/category.png" class="v_panel" alt="category">
+                    <h5 class="mb-0">Category</h5>
+                </a>
+            </div>
+            <div class="m-item">
+                <a href="#" class="nav-item">
+                    <img src="./images/category.png" class="v_panel" alt="saved">
+                    <h5 class="mb-0">Saved</h5>
+                </a>
+            </div>
+            <div class="m-item">
+                <a href="#" class="nav-item">
+                    <img src="./images/category.png" class="v_panel" alt="library">
+                    <h5 class="mb-0">Library</h5>
+                </a>
+            </div>
+            <div class="bottom-items">
+                <div class="m-item">
+                    <a href="#" class="nav-item">
+                        <img src="./images/category.png" class="v_panel" alt="category">
+                        <h5 class="mb-0">Settings</h5>
+                    </a>
+                </div>
+                <div class="m-item">
+                    <a href="#" class="nav-item">
+                        <img src="./images/category.png" class="v_panel" alt="saved">
+                        <h5 class="mb-0">Support</h5>
+                    </a>
+                </div>
+                <div class="m-item">
+                    <a href="#" class="nav-item">
+                        <img src="./images/category.png" class="v_panel" alt="library">
+                        <h5 class="mb-0">Logout</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+      `;
+  }
+}
+
+customElements.define("sidebar-panel", SideBar);
